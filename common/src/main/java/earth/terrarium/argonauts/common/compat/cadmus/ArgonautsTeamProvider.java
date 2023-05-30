9dd4293c01775251a2aba5c2d183e3c3386fb3cd
Alex Nijjar <alexander.nijjar@icloud.com>
2023-05-30 14:12:00 -0500

    1.20.x

--- common/src/main/java/earth/terrarium/argonauts/common/compat/cadmus/ArgonautsTeamProvider.java
+++ common/src/main/java/earth/terrarium/argonauts/common/compat/cadmus/ArgonautsTeamProvider.java
@@ -1,135 +1,135 @@
-//package earth.terrarium.argonauts.common.compat.cadmus;
-//
-//import com.mojang.authlib.GameProfile;
-//import earth.terrarium.argonauts.common.handlers.base.MemberPermissions;
-//import earth.terrarium.argonauts.common.handlers.guild.Guild;
-//import earth.terrarium.argonauts.common.handlers.guild.GuildHandler;
-//import earth.terrarium.argonauts.common.handlers.guild.members.GuildMember;
-//import earth.terrarium.argonauts.common.handlers.guild.members.GuildMembers;
-//import earth.terrarium.argonauts.common.handlers.party.Party;
-//import earth.terrarium.argonauts.common.handlers.party.PartyHandler;
-//import earth.terrarium.cadmus.api.claims.InteractionType;
-//import earth.terrarium.cadmus.api.teams.TeamProvider;
-//import earth.terrarium.cadmus.common.claims.ClaimHandler;
-//import net.minecraft.ChatFormatting;
-//import net.minecraft.Optionull;
-//import net.minecraft.core.BlockPos;
-//import net.minecraft.network.chat.Component;
-//import net.minecraft.server.MinecraftServer;
-//import net.minecraft.world.entity.Entity;
-//import net.minecraft.world.level.Explosion;
-//import org.jetbrains.annotations.Nullable;
-//
-//import java.util.HashSet;
-//import java.util.Set;
-//import java.util.UUID;
-//
-//public class ArgonautsTeamProvider implements TeamProvider {
-//
-//    @Override
-//    public Set<GameProfile> getTeamMembers(String id, MinecraftServer server) {
-//        Guild guild = GuildHandler.get(server, UUID.fromString(id));
-//        Set<GameProfile> profiles = new HashSet<>();
-//        if (guild == null) return profiles;
-//        for (GuildMember player : guild.members()) {
-//            server.getProfileCache().get(player.profile().getId()).ifPresent(profiles::add);
-//        }
-//        return profiles;
-//    }
-//
-//    @Override
-//    @Nullable
-//    public Component getTeamName(String id, MinecraftServer server) {
-//        var guild = GuildHandler.get(server, UUID.fromString(id));
-//        if (guild == null) {
-//            var profile = server.getProfileCache().get(UUID.fromString(id));
-//            return profile.map(p -> Component.literal(p.getName())).orElse(null);
-//        }
-//        return guild.getDisplayName();
-//    }
-//
-//    @Override
-//    public String getTeamId(MinecraftServer server, UUID player) {
-//        var profile = server.getProfileCache().get(player).orElse(null);
-//        if (profile == null) return player.toString();
-//        var guild = GuildHandler.getPlayerGuild(server, player);
-//        if (guild == null) return player.toString();
-//        return guild.id().toString();
-//    }
-//
-//    @Override
-//    public boolean isMember(String id, MinecraftServer server, UUID player) {
-//        var profile = server.getProfileCache().get(player).orElse(null);
-//        if (profile == null) return false;
-//        var guild = GuildHandler.get(server, UUID.fromString(id));
-//        if (guild == null) return id.equals(player.toString());
-//        return guild.members().isMember(player);
-//    }
-//
-//    @Override
-//    public ChatFormatting getTeamColor(String id, MinecraftServer server) {
-//        var guild = GuildHandler.get(server, UUID.fromString(id));
-//        var result = Optionull.mapOrDefault(guild, Guild::getColor, ChatFormatting.AQUA);
-//        return result == ChatFormatting.RESET ? ChatFormatting.AQUA : result;
-//    }
-//
-//    @Override
-//    public boolean canBreakBlock(String id, MinecraftServer server, BlockPos pos, UUID player) {
-//        return hasPermission(MemberPermissions.BREAK_BLOCKS, id, server, player);
-//    }
-//
-//    @Override
-//    public boolean canPlaceBlock(String id, MinecraftServer server, BlockPos pos, UUID player) {
-//        return hasPermission(MemberPermissions.PLACE_BLOCKS, id, server, player);
-//    }
-//
-//    @Override
-//    public boolean canExplodeBlock(String id, MinecraftServer server, BlockPos pos, Explosion explosion, UUID player) {
-//        return hasPermission(MemberPermissions.EXPLODE_BLOCKS, id, server, player);
-//    }
-//
-//    @Override
-//    public boolean canInteractWithBlock(String id, MinecraftServer server, BlockPos pos, InteractionType type, UUID player) {
-//        return hasPermission(MemberPermissions.INTERACT_WITH_BLOCKS, id, server, player);
-//    }
-//
-//    @Override
-//    public boolean canInteractWithEntity(String id, MinecraftServer server, Entity entity, UUID player) {
-//        return hasPermission(MemberPermissions.INTERACT_WITH_ENTITIES, id, server, player);
-//    }
-//
-//    @Override
-//    public boolean canDamageEntity(String id, MinecraftServer server, Entity entity, UUID player) {
-//        return hasPermission(MemberPermissions.DAMAGE_ENTITIES, id, server, player);
-//    }
-//
-//    private boolean hasPermission(String perm, String id, MinecraftServer server, UUID player) {
-//        if (isMember(id, server, player)) return true;
-//        Guild guild = GuildHandler.get(server, UUID.fromString(id));
-//        if (guild == null) return false;
-//        if (guild.settings().allowFakePlayers() && guild.members() instanceof GuildMembers members && members.fakePlayers().contains(player)) {
-//            return true;
-//        }
-//
-//        Party party = PartyHandler.getPlayerParty(player);
-//        if (party == null) return false;
-//        var member = party.members().get(player);
-//        if (member == null) return false;
-//        if (!member.hasPermission(perm)) return false;
-//
-//        var guildMember = guild.members().get(party.members().getLeader().profile().getId());
-//        if (guildMember == null) return false;
-//        return party.members().isMember(player) && guildMember.hasPermission(MemberPermissions.TEMPORARY_GUILD_PERMISSIONS);
-//    }
-//
-//    public void onTeamChanged(MinecraftServer server, String id, UUID player) {
-//        this.onTeamChanged(server, id);
-//        server.getAllLevels().forEach(l -> ClaimHandler.clear(l, player.toString()));
-//    }
-//
-//    @Override
-//    public void onTeamRemoved(MinecraftServer server, String id) {
-//        TeamProvider.super.onTeamRemoved(server, id);
-//        server.getAllLevels().forEach(l -> ClaimHandler.clear(l, id));
-//    }
-//}
+package earth.terrarium.argonauts.common.compat.cadmus;
+
+import com.mojang.authlib.GameProfile;
+import earth.terrarium.argonauts.common.handlers.base.MemberPermissions;
+import earth.terrarium.argonauts.common.handlers.guild.Guild;
+import earth.terrarium.argonauts.common.handlers.guild.GuildHandler;
+import earth.terrarium.argonauts.common.handlers.guild.members.GuildMember;
+import earth.terrarium.argonauts.common.handlers.guild.members.GuildMembers;
+import earth.terrarium.argonauts.common.handlers.party.Party;
+import earth.terrarium.argonauts.common.handlers.party.PartyHandler;
+import earth.terrarium.cadmus.api.claims.InteractionType;
+import earth.terrarium.cadmus.api.teams.TeamProvider;
+import earth.terrarium.cadmus.common.claims.ClaimHandler;
+import net.minecraft.ChatFormatting;
+import net.minecraft.Optionull;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Explosion;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class ArgonautsTeamProvider implements TeamProvider {
+
+    @Override
+    public Set<GameProfile> getTeamMembers(String id, MinecraftServer server) {
+        Guild guild = GuildHandler.get(server, UUID.fromString(id));
+        Set<GameProfile> profiles = new HashSet<>();
+        if (guild == null) return profiles;
+        for (GuildMember player : guild.members()) {
+            profiles.add(player.profile());
+        }
+        return profiles;
+    }
+
+    @Override
+    @Nullable
+    public Component getTeamName(String id, MinecraftServer server) {
+        var guild = GuildHandler.get(server, UUID.fromString(id));
+        if (guild == null) {
+            var profile = server.getProfileCache().get(UUID.fromString(id));
+            return profile.map(p -> Component.literal(p.getName())).orElse(null);
+        }
+        return guild.getDisplayName();
+    }
+
+    @Override
+    public String getTeamId(MinecraftServer server, UUID player) {
+        var profile = server.getProfileCache().get(player).orElse(null);
+        if (profile == null) return ClaimHandler.PLAYER_PREFIX + player.toString();
+        var guild = GuildHandler.getPlayerGuild(server, player);
+        if (guild == null) return ClaimHandler.PLAYER_PREFIX + player.toString();
+        return ClaimHandler.TEAM_PREFIX + guild.id().toString();
+    }
+
+    @Override
+    public boolean isMember(String id, MinecraftServer server, UUID player) {
+        var profile = server.getProfileCache().get(player).orElse(null);
+        if (profile == null) return false;
+        var guild = GuildHandler.get(server, UUID.fromString(id));
+        if (guild == null) return id.equals(player.toString());
+        return guild.members().isMember(player);
+    }
+
+    @Override
+    public ChatFormatting getTeamColor(String id, MinecraftServer server) {
+        var guild = GuildHandler.get(server, UUID.fromString(id));
+        var result = Optionull.mapOrDefault(guild, Guild::getColor, ChatFormatting.AQUA);
+        return result == ChatFormatting.RESET ? ChatFormatting.AQUA : result;
+    }
+
+    @Override
+    public boolean canBreakBlock(String id, MinecraftServer server, BlockPos pos, UUID player) {
+        return hasPermission(MemberPermissions.BREAK_BLOCKS, id, server, player);
+    }
+
+    @Override
+    public boolean canPlaceBlock(String id, MinecraftServer server, BlockPos pos, UUID player) {
+        return hasPermission(MemberPermissions.PLACE_BLOCKS, id, server, player);
+    }
+
+    @Override
+    public boolean canExplodeBlock(String id, MinecraftServer server, BlockPos pos, Explosion explosion, UUID player) {
+        return hasPermission(MemberPermissions.EXPLODE_BLOCKS, id, server, player);
+    }
+
+    @Override
+    public boolean canInteractWithBlock(String id, MinecraftServer server, BlockPos pos, InteractionType type, UUID player) {
+        return hasPermission(MemberPermissions.INTERACT_WITH_BLOCKS, id, server, player);
+    }
+
+    @Override
+    public boolean canInteractWithEntity(String id, MinecraftServer server, Entity entity, UUID player) {
+        return hasPermission(MemberPermissions.INTERACT_WITH_ENTITIES, id, server, player);
+    }
+
+    @Override
+    public boolean canDamageEntity(String id, MinecraftServer server, Entity entity, UUID player) {
+        return hasPermission(MemberPermissions.DAMAGE_ENTITIES, id, server, player);
+    }
+
+    private boolean hasPermission(String perm, String id, MinecraftServer server, UUID player) {
+        if (isMember(id, server, player)) return true;
+        Guild guild = GuildHandler.get(server, UUID.fromString(id));
+        if (guild == null) return false;
+        if (guild.settings().allowFakePlayers() && guild.members() instanceof GuildMembers members && members.fakePlayers().contains(player)) {
+            return true;
+        }
+
+        Party party = PartyHandler.getPlayerParty(player);
+        if (party == null) return false;
+        var member = party.members().get(player);
+        if (member == null) return false;
+        if (!member.hasPermission(perm)) return false;
+
+        var guildMember = guild.members().get(party.members().getLeader().profile().getId());
+        if (guildMember == null) return false;
+        return party.members().isMember(player) && guildMember.hasPermission(MemberPermissions.TEMPORARY_GUILD_PERMISSIONS);
+    }
+
+    public void onTeamChanged(MinecraftServer server, String id, UUID player) {
+        this.onTeamChanged(server, id);
+        server.getAllLevels().forEach(l -> ClaimHandler.clear(l, player.toString()));
+    }
+
+    @Override
+    public void onTeamRemoved(MinecraftServer server, String id) {
+        TeamProvider.super.onTeamRemoved(server, id);
+        server.getAllLevels().forEach(l -> ClaimHandler.clear(l, ClaimHandler.TEAM_PREFIX + id));
+    }
+}
--- common/src/main/java/earth/terrarium/argonauts/common/compat/cadmus/CadmusIntegration.java
+++ common/src/main/java/earth/terrarium/argonauts/common/compat/cadmus/CadmusIntegration.java
@@ -2,43 +2,46 @@ package earth.terrarium.argonauts.common.compat.cadmus;
 
 import earth.terrarium.argonauts.Argonauts;
 import earth.terrarium.argonauts.common.handlers.guild.Guild;
+import earth.terrarium.cadmus.api.teams.TeamProviderApi;
+import earth.terrarium.cadmus.common.claims.ClaimHandler;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 
 public class CadmusIntegration {
     public static final ResourceLocation ARGONAUTS_ID = new ResourceLocation(Argonauts.MOD_ID, Argonauts.MOD_ID);
 
     public static void init() {
-//        TeamProviderApi.API.register(ARGONAUTS_ID, new ArgonautsTeamProvider());
-//        TeamProviderApi.API.setSelected(ARGONAUTS_ID);
+        TeamProviderApi.API.register(ARGONAUTS_ID, new ArgonautsTeamProvider());
+        TeamProviderApi.API.setSelected(ARGONAUTS_ID);
     }
 
     public static void addToCadmusTeam(ServerPlayer player, String id) {
-//        if (TeamProviderApi.API.getSelected() instanceof ArgonautsTeamProvider provider) {
-//            provider.onTeamChanged(player.server, id, player.getUUID());
-//        }
+        if (TeamProviderApi.API.getSelected() instanceof ArgonautsTeamProvider provider) {
+            provider.onTeamChanged(player.server, id, player.getUUID());
+        }
     }
 
     public static void removeFromCadmusTeam(ServerPlayer player, String id) {
-//        if (TeamProviderApi.API.getSelected() instanceof ArgonautsTeamProvider provider) {
-//            provider.onTeamChanged(player.server, id, player.getUUID());
-//        }
+        if (TeamProviderApi.API.getSelected() instanceof ArgonautsTeamProvider provider) {
+            provider.onTeamChanged(player.server, id, player.getUUID());
+        }
     }
 
     public static void disbandCadmusTeam(Guild guild, MinecraftServer server) {
-//        if (TeamProviderApi.API.getSelected() instanceof ArgonautsTeamProvider provider) {
-//            provider.onTeamRemoved(server, guild.id().toString());
-//        }
+        if (TeamProviderApi.API.getSelected() instanceof ArgonautsTeamProvider provider) {
+            provider.onTeamRemoved(server, guild.id().toString());
+        }
     }
 
     public static int getChunksForGuild(Guild guild, MinecraftServer server) {
         int claims = 0;
-//        for (ServerLevel allLevel : server.getAllLevels()) {
-//            var teamClaims = ClaimHandler.getTeamClaims(allLevel, guild.id().toString());
-//            if (teamClaims == null) continue;
-//            claims += teamClaims.size();
-//        }
+        for (ServerLevel allLevel : server.getAllLevels()) {
+            var teamClaims = ClaimHandler.getTeamClaims(allLevel, guild.id().toString());
+            if (teamClaims == null) continue;
+            claims += teamClaims.size();
+        }
         return claims;
     }
 }
