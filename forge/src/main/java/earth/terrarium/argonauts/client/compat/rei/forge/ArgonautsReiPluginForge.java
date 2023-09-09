8097103dc2f767adbacb8be33600aecab58b7cde
Alex Nijjar <alexander.nijjar@icloud.com>
2023-09-09 13:53:35 -0500

    Fix REI dumb on forge

---
+++ forge/src/main/java/earth/terrarium/argonauts/client/compat/rei/forge/ArgonautsReiPluginForge.java
@@ -0,0 +1,8 @@
+package earth.terrarium.argonauts.client.compat.rei.forge;
+
+import earth.terrarium.argonauts.client.compat.rei.ArgonautsReiClientPlugin;
+import me.shedaniel.rei.forge.REIPluginClient;
+
+@REIPluginClient
+public class ArgonautsReiPluginForge extends ArgonautsReiClientPlugin {
+}
